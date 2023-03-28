2035175eff5d49ccf97ce94fe37dfcf4d0127f83
LaurynG1 <l.griffin1@ufl.edu>
2023-03-28 19:42:12 -0400

    added comments to file

--- encoder.py
+++ encoder.py
@@ -1,6 +1,6 @@
-def encode_password(password):
+def encode_password(password): # encode password function
     encoded_password = ''
-    password_nums = [int(i) for i in passwrd]
+    password_nums = [int(i) for i in password]
     for i in password_nums:
         i += 3
         encoded_password += str(i)
@@ -17,13 +17,13 @@ if __name__ == "__main__":
               '3. Quit')
         menu_sel = int(input('Please enter an option: '))
         if menu_sel == 1:
-            global passwrd
+            global passwrd  # global use to use in other menu option
             passwrd = input('Please enter your password to encode: ')
-            global encoder
+            global encoder  # global use to use in other menu option
             encoder = encode_password(passwrd)
             print('Your password has been encoded and stored!')
         if menu_sel == 2:
-            decoder = decode_password(passwrd)
+            decoder = decode_password(passwrd)  # decode function will be added by partner
             print('The encoded password is', encoder,
                   ', and the original password is', decoder, '.')
         if menu_sel == 3:
